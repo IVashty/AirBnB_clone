42041fad216680ec239b59af51ee209d8e135c61
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-11-24 07:51:54 -0800

    base model commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,74 @@
 #!/usr/bin/python3
+"""Defines all common attributes/methods for other classes
 """
-Super class that all other objects inherit from.
-"""
-
-from uuid import uuid4
+import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    """
-    __init__ method -> should handles input from other methods from BaseModel
-    __str__ method -> should returns class name, id, and a dictionary
-    """
+    """Base class for all models"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of a Base instance.
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+        if kwargs:
+            dtime_format = '%Y-%m-%dT%H:%M:%S.%f'
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(
+                        kwargs['created_at'], dtime_format)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                        kwargs['updated_at'], dtime_format)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)  # as instructed also in task 5
 
     def __str__(self):
-        """
-        Returns the class name, id, and the dictionary to be printed
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """Returns a readable string representation
+        of BaseModel instances"""
 
-        def __init__(self, *args, **kwargs):
-            """
-            Initiating the BaseModel class to be able to accept arguments\
-         Converting self.created_at and self.updated_at into datetime objects
-            if the incoming argument is a dictionary
-            If they aren't a dictionary, call a function that turns them into
-            a dictionary.
-            """
+        clsName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
 
-        formating = "%Y-%m-%d %H:%M:%S.%f"
-        change = 0
-        for arg in args:
-            if type(arg) is dict:
-                change = 1
-                self.created_at = datetime.strptime(arg["created_at"], formating)
-                self.updated_at = datetime.strptime(arg["updated_at"], formating)
-                self.__dict__ = args
-            if change == 0:
-                self.created_at = datetime.now()
-                self.id = str(uuid4())
-                from models.__init__ import storage
+    def save(self):
+        """Updates the public instance attribute updated_at
+        with the current datetime"""
 
-                storage.new(self)
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary that contains all
+        keys/values of the instance"""
+        my_dict = self.__dict__.copy()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['__class__'] = self.__class__.__name__
+        # OR
+        # my_dict = dict()
+        # my_dict['__class__'] = self.__class__.__name__
+        # for key, value in self.__dict__.items():
+        #    if key in ('created_at', 'updated_at'):
+        #        my_dict[key] = value.isoformat()
+        #    else:
+        #        my_dict[key] = value
+        return my_dict
+        # my_dict = dict()
+        # my_dict['__class__'] = self.__class__.__name__
+        # for key, value in self.__dict__.items():
+        #    if type(value) is datetime:
+        #        my_dict[key] = value.isoformat()
+        #    else:
+        #        my_dict[key] = value
+        # return my_dict
