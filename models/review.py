320414fc45065a78651f5b120677b3dc4c362e69
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-11-26 12:26:01 -0800

    review updated

--- models/review.py
+++ models/review.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-""" review module"""
-
+"""
+Module Review class
+"""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ Review model"""
+    """
+    Inherits from BaseModel
+    Public class attributes:
+        place_id:            (str) will be Place.id
+        user_id:             (str) will be User.id
+        text:                (str)
+    """
     place_id = ""
     user_id = ""
     text = ""
